7011834b81268823eebab8d9478d7c7ba2be51fa
Christopher Sae Glood <christopherglood@gmail.com>
2024-05-13 09:26:22 -0400

    disable arrows on insert mode and keymaps for visual

--- lua/disable-arrows.lua
+++ lua/disable-arrows.lua
@@ -3,3 +3,8 @@ vim.api.nvim_set_keymap('', '<Up>', '<Nop>', {noremap = true, silent = true})
 vim.api.nvim_set_keymap('', '<Down>', '<Nop>', {noremap = true, silent = true})
 vim.api.nvim_set_keymap('', '<Left>', '<Nop>', {noremap = true, silent = true})
 vim.api.nvim_set_keymap('', '<Right>', '<Nop>', {noremap = true, silent = true})
+-- Disable arrow keys in INSERT
+vim.api.nvim_set_keymap('i', '<Up>', '<Nop>', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('i', '<Down>', '<Nop>', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('i', '<Left>', '<Nop>', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('i', '<Right>', '<Nop>', {noremap = true, silent = true})
--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -2,16 +2,19 @@
 local map = vim.api.nvim_set_keymap
 local builtin = require('telescope.builtin')
 -- movement on insert mode
-vim.api.nvim_set_keymap('i', '<C-h>', '<Left>', {noremap = true, silent = true})
-vim.api.nvim_set_keymap('i', '<C-j>', '<Down>', {noremap = true, silent = true})
-vim.api.nvim_set_keymap('i', '<C-k>', '<Up>', {noremap = true, silent = true})
-vim.api.nvim_set_keymap('i', '<C-l>', '<Right>', {noremap = true, silent = true})
+map('i', '<C-h>', '<Left>', { noremap = true, silent = true })
+map('i', '<C-j>', '<Down>', { noremap = true, silent = true })
+map('i', '<C-k>', '<Up>', { noremap = true, silent = true })
+map('i', '<C-l>', '<Right>', { noremap = true, silent = true })
 -- keymaps
 map('n', '<Space>qq', ':wa<CR>:qa<CR>', { noremap = true, silent = true }) -- Space+qq: Cerrar sin Guardar
-map("n", "<S-k>", ":m .-2<CR>==", { silent = true }) -- Shift+j: Mover línea hacia arriba
-map("n", "<S-j>", ":m .+1<CR>==", { silent = true }) -- Shift+j: Mover línea hacia abajo
 map('n', '<C-w>', ':w<CR>', { noremap = true, silent = false }) -- Control+w: Guardar (:w)
 map('n', '<C-q>', ':q<CR>', { noremap = true, silent = false }) -- Control+q: Cerrar (:q)
+-- move line and selected block on visual mode
+map("n", "<S-k>", ":m .-2<CR>==", { silent = true }) -- Shift+k: Mover línea hacia arriba
+map("n", "<S-j>", ":m .+1<CR>==", { silent = true }) -- Shift+j: Mover línea hacia abajo
+map('v', "<S-k>", ":move '<-2<CR>gv=gv", { noremap = true }) -- Shift+k: Mover bloque seleccionado hacia arriba
+map('v', "<S-j>", ":move '>+1<CR>gv=gv", { noremap = true }) -- Shift+j: Mover bloque seleccionado hacia abajo
 -- telescope
 vim.keymap.set('n', '<Space>ff', builtin.find_files, {})
 vim.keymap.set('n', '<Space>fg', builtin.live_grep, {})
